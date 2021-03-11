6c7d2267a0240e6c84fb28099c420a6f0210db7a
Yangchen <a2045751@163.com>
2021-03-12 01:01:28 +0800

    Get album title

--- GetPictures/GetPictures/spiders/img.py
+++ GetPictures/GetPictures/spiders/img.py
@@ -1,3 +1,4 @@
+import re
 import scrapy
 
 
@@ -10,5 +11,12 @@ class ImgSpider(scrapy.Spider):
         # //*[@id="imgList"]/ul/li[1]
         page_urls = response.xpath('//*[@id="imgList"]/ul/li/a/@href').extract()
         for page_url in page_urls:
-            print(response.urljoin(page_url))
+            page_url = response.urljoin(page_url)
+            print(page_url)
+            yield scrapy.Request(page_url, callback=self.img_parse, dont_filter=True)
 
+    def img_parse(self, response):
+        # /html/body/div[3]/div[2]/article/section[1]/div[1]/h1
+        title = response.xpath('//div[@class="ArticleH1"]//h1/text()').extract_first()
+        title = re.sub('\d+/\d+', '', title).strip()
+        print(title)
