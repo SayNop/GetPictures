f6b271f5b84b6a65277c17c5407d275a65eddd7c
Yangchen <a2045751@163.com>
2021-03-12 00:55:27 +0800

    Get album link

--- GetPictures/GetPictures/spiders/img.py
+++ GetPictures/GetPictures/spiders/img.py
@@ -7,4 +7,8 @@ class ImgSpider(scrapy.Spider):
     start_urls = ['https://www.duotoo.com/rentiyishu/']
 
     def parse(self, response):
-        pass
+        # //*[@id="imgList"]/ul/li[1]
+        page_urls = response.xpath('//*[@id="imgList"]/ul/li/a/@href').extract()
+        for page_url in page_urls:
+            print(response.urljoin(page_url))
+
